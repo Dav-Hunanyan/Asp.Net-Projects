4c8f025c4a270cc466ee392d1609935503004579
Dav <dav040201@gmail.com>
2022-06-30 14:25:35 +0400

    Asp.Net Projects

---
+++ MvcMovies/MvcMovies/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovies.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        [Column(TypeName="decimal(18,2)")]
+        public decimal Price { get; set; }
+    }
+}
